2e3755a33383d05c443eab28c5b8f6a4a2d210c2
andydenenberg <andydenenberg@gmail.com>
2012-06-15 09:27:01 -0500

    added PW confirmable and mail

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'jquery-rails'
 
 gem 'jquery_datepicker'
 
+gem 'sendgrid'
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Talon::Application.initialize!
+
+config = YAML.load(File.read('../../../desktop/config.yml'))   
+
+ActionMailer::Base.smtp_settings = {
+  :user_name =>  config['user_name'],
+  :password => config['password'],
+#  :user_name =>  ENV['SENDGRID_USERNAME'] ,
+#  :password => ENV['SENDGRID_PASSWORD'] ,
+  :domain => "ospreypointpartners.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
